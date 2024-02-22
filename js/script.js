dcda45389203dd7a97d01e4c1c6f762f5b5d9ccf
chevyuxu <miyulll92@gmail.com>
2024-02-22 23:20:00 +0800

    textContent

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
+if (window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', function (event) {
+        var a = document.getElementById('alpha'),
+            b = document.getElementById('beta'),
+            g = document.getElementById('gamma'),
+            alpha = event.alpha,
+            beta = event.beta,
+            gamma = event.gamma;
 
-if(window.DeviceOrientationEvent) {
-  window.addEventListener('deviceorientation', function(event) {
-  var a = document.getElementById('alpha'),
-        b = document.getElementById('beta'),
-        g = document.getElementById('gamma'),
-        alpha = event.alpha,
-        beta = event.beta,
-        gamma = event.gamma;
-
-  a.innerHTML = Math.round(alpha);
-  b.innerHTML = Math.round(beta);
-  g.innerHTML = Math.round(gamma);
+        // 使用textContent屬性來更新元素的內容
+        a.textContent = Math.round(alpha);
+        b.textContent = Math.round(beta);
+        g.textContent = Math.round(gamma);
 
     }, false);
-}else{
-    document.querySelector('body').innerHTML = '你的瀏覽器不支援喔';
+} else {
+    document.querySelector('body').textContent = '你的瀏覽器不支援喔';
 }
